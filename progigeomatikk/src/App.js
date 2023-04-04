33c8393b55508f701cc064b976212da0d5402a32
INGRID HECK <ingridlheck@gmail.com>
2023-04-04 16:26:21 +0200

    Refresh and remove access token from repository

--- progigeomatikk/src/App.js
+++ progigeomatikk/src/App.js
@@ -6,16 +6,16 @@ import { useState } from 'react';
 
 function App() {
   const [lng, setlng] = useState(10.421906);
-  const [lat, setlat] = useState(63.446827);
+  const [lat, setlat] = useState(63.406827);
 
   return (
     <div className="App">
-      <h1>Hei</h1>
+
       <Map
-      mapboxAccessToken='pk.eyJ1IjoiaWxsaGVjayIsImEiOiJja3poNDlqODAxNzJxMnZueXo1a3BrNHF0In0.OXM8RqhO-dOV7wG6-_zkTQ'
+      mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       style={
-        {width: "500px",
-        height: "500px",
+        {width: "70vw",
+        height: "50vw",
         borderRadius: "15px",
         border: '2px solid red'}
       }
